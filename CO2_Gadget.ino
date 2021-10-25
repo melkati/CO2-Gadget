3567517f4e3ee213a1c6108adb539b4d8cfec05b
eMariete.com <melkati@users.noreply.github.com>
2021-10-25 20:39:31 +0200

    Clang-format protect comments

--- CO2_Gadget.ino
+++ CO2_Gadget.ino
@@ -1,3 +1,4 @@
+// clang-format off
 /*****************************************************************************************************/
 /*********                   GENERAL GLOBAL DEFINITIONS AND OPTIONS                          *********/
 /*****************************************************************************************************/
@@ -14,21 +15,24 @@
 /**/ // #define ALTERNATIVE_I2C_PINS   // For the compact build as shown at https://emariete.com/medidor-co2-display-tft-color-ttgo-t-display-sensirion-scd30/
 /**/ #endif
 /*****************************************************************************************************/
+// clang-format on
 
 #ifdef BUILD_GIT
-# undef BUILD_GIT
+#undef BUILD_GIT
 #endif // ifdef BUILD_GIT
 #define BUILD_GIT __DATE__
 
 #include <Wire.h>
-#include "soc/soc.h"           // disable brownout problems
-#include "soc/rtc_cntl_reg.h"  // disable brownout problems
+#include "soc/soc.h" // disable brownout problems
+#include "soc/rtc_cntl_reg.h" // disable brownout problems
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                                   SETUP SENSORS                                   *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 
 #include <Sensors.hpp>
 
@@ -40,7 +44,7 @@
 #define I2C_SCL 22
 #endif
 
-bool pendingCalibration = false;
+    bool pendingCalibration = false;
 bool newReadingsAvailable = false;
 uint16_t calibrationValue = 415;
 uint16_t customCalibrationValue = 415;
@@ -50,150 +54,165 @@ uint16_t altidudeMeters = 600;
 bool autoSelfCalibration = false;
 
 uint16_t co2 = 0;
-float temp, hum  = 0; 
+float temp, hum = 0;
 
-
-uint16_t  co2OrangeRange = 700; // Default CO2 ppm concentration threshold to display values in orange (user can change on menu and save on preferences)
-uint16_t  co2RedRange = 1000;   // Default CO2 ppm concentration threshold to display values in red (user can change on menu and save on preferences)
-
+uint16_t co2OrangeRange =
+    700; // Default CO2 ppm concentration threshold to display values in orange
+         // (user can change on menu and save on preferences)
+uint16_t co2RedRange =
+    1000; // Default CO2 ppm concentration threshold to display values in red
+          // (user can change on menu and save on preferences)
 
 void onSensorDataOk() {
-    Serial.print("-->[MAIN] CO2: " + sensors.getStringCO2());
-    Serial.print(" CO2humi: " + String(sensors.getCO2humi()));
-    Serial.print(" CO2temp: " + String(sensors.getCO2temp()));
+  Serial.print("-->[MAIN] CO2: " + sensors.getStringCO2());
+  Serial.print(" CO2humi: " + String(sensors.getCO2humi()));
+  Serial.print(" CO2temp: " + String(sensors.getCO2temp()));
 
-    Serial.print(" H: " + String(sensors.getHumidity()));
-    Serial.println(" T: " + String(sensors.getTemperature()));
+  Serial.print(" H: " + String(sensors.getHumidity()));
+  Serial.println(" T: " + String(sensors.getTemperature()));
 
-    co2  = sensors.getCO2();
-    temp = sensors.getCO2temp();
-    hum  = sensors.getCO2humi();
-    newReadingsAvailable = true;
+  co2 = sensors.getCO2();
+  temp = sensors.getCO2temp();
+  hum = sensors.getCO2humi();
+  newReadingsAvailable = true;
 }
 
-void onSensorDataError(const char* msg) {
-    Serial.println(msg);
-}
+void onSensorDataError(const char *msg) { Serial.println(msg); }
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                              SETUP BLE FUNCTIONALITY                              *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #ifdef SUPPORT_BLE
 #include "Sensirion_GadgetBle_Lib.h"
 GadgetBle gadgetBle = GadgetBle(GadgetBle::DataType::T_RH_CO2);
 #endif
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                           INCLUDE WIFI FUNCTIONALITY                              *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #ifdef SUPPORT_WIFI
 #include <CO2_Gadget_WIFI>
 #endif
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                            SETUP MQTT FUNCTIONALITY                               *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #if defined SUPPORT_MQTT
 #include "CO2_Gadget_MQTT.h"
 #endif
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                             SETUP OTA FUNCTIONALITY                               *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #if defined SUPPORT_OTA
 #include <AsyncElegantOTA.h>
 #endif
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                           INCLUDE BATTERY FUNCTIONALITY                           *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #define ADC_PIN 34
 int vref = 1100;
 #include "CO2_Gadget_Battery.h"
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********               INCLUDE OLED DISPLAY FUNCTIONALITY (UNFINISHED WIP)                 *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #if defined SUPPORT_OLED
 #include <CO2_Gadget_OLED.h>
 #endif
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                      INCLUDE TFT DISPLAY FUNCTIONALITY                            *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #if defined SUPPORT_TFT
 #include "CO2_Gadget_TFT.h"
 #endif
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********        FUNCTIONALITY TO STORE PREFERENCES IN NON VOLATILE MEMORY                  *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #include "CO2_Gadget_Preferences.h"
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                         INCLUDE MENU FUNCIONALITY                                 *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 #include "CO2_Gadget_Menu.h"
 
+// clang-format off
 /*****************************************************************************************************/
 /*********                                                                                   *********/
 /*********                      SETUP PUSH BUTTONS FUNCTIONALITY                             *********/
 /*********                                                                                   *********/
 /*****************************************************************************************************/
+// clang-format on
 // Button2 button;
-// #define BUTTON_PIN  35 // Menu button (Press > 1500ms for calibration, press > 500ms to show battery voltage)
-// void longpress(Button2& btn);
+// #define BUTTON_PIN  35 // Menu button (Press > 1500ms for calibration, press
+// > 500ms to show battery voltage) void longpress(Button2& btn);
 #define LONGCLICK_MS 300 // https://github.com/LennartHennigs/Button2/issues/10
-#define BTN_UP 35 // Pinnumber for button for up/previous and select / enter actions
+#define BTN_UP                                                                 \
+  35 // Pinnumber for button for up/previous and select / enter actions
 #define BTN_DWN 0 // Pinnumber for button for down/next and back / exit actions
 #include "Button2.h"
-Button2 btnUp(BTN_UP); // Initialize the up button
+Button2 btnUp(BTN_UP);   // Initialize the up button
 Button2 btnDwn(BTN_DWN); // Initialize the down button
 
-void button_init()
-{
-    btnUp.setLongClickHandler([](Button2 & b) {        
-        nav.doNav(enterCmd);
-    });
-    
-    btnUp.setClickHandler([](Button2 & b) {
-       // Up
-       nav.doNav(downCmd);
-    });
+void button_init() {
+  btnUp.setLongClickHandler([](Button2 &b) { nav.doNav(enterCmd); });
 
-    btnDwn.setLongClickHandler([](Button2 & b) {
-        nav.doNav(escCmd);
-    });
-    
-    btnDwn.setClickHandler([](Button2 & b) {
-        // Down
-        nav.doNav(upCmd);
-    });
+  btnUp.setClickHandler([](Button2 &b) {
+    // Up
+    nav.doNav(downCmd);
+  });
+
+  btnDwn.setLongClickHandler([](Button2 &b) { nav.doNav(escCmd); });
+
+  btnDwn.setClickHandler([](Button2 &b) {
+    // Down
+    nav.doNav(upCmd);
+  });
 }
 
-void button_loop()
-{
-    // Check for button presses
-    btnUp.loop();
-    btnDwn.loop();
+void button_loop() {
+  // Check for button presses
+  btnUp.loop();
+  btnDwn.loop();
 }
 
 /*****************************************************************************************************/
@@ -201,8 +220,7 @@ void button_loop()
 static int64_t lastMmntTime = 0;
 static int startCheckingAfterUs = 1900000;
 
-void initMQTT()
-{
+void initMQTT() {
 #ifdef SUPPORT_MQTT
   mqttClient.setServer(mqtt_server, 1883);
   mqttClient.setCallback(callback);
@@ -210,42 +228,41 @@ void initMQTT()
 #endif
 }
 
-void processPendingCommands()
-{
+void processPendingCommands() {
   if (pendingCalibration == true) {
     if (calibrationValue != 0) {
       printf("Calibrating CO2 sensor at %d PPM\n", calibrationValue);
       pendingCalibration = false;
       sensors.setCO2RecalibrationFactor(calibrationValue);
-    }
-    else
-    {
-      printf("Avoiding calibrating CO2 sensor with invalid value at %d PPM\n", calibrationValue);
+    } else {
+      printf("Avoiding calibrating CO2 sensor with invalid value at %d PPM\n",
+             calibrationValue);
       pendingCalibration = false;
     }
   }
 
   if (pendingAmbientPressure == true) {
     if (ambientPressureValue != 0) {
-      printf("Setting AmbientPressure for CO2 sensor at %d mbar\n", ambientPressureValue);
+      printf("Setting AmbientPressure for CO2 sensor at %d mbar\n",
+             ambientPressureValue);
       pendingAmbientPressure = false;
       sensors.scd30.setAmbientPressure(ambientPressureValue);
-    }
-    else
-    {
-      printf("Avoiding setting AmbientPressure for CO2 sensor with invalid value at %d mbar\n", ambientPressureValue);
+    } else {
+      printf("Avoiding setting AmbientPressure for CO2 sensor with invalid "
+             "value at %d mbar\n",
+             ambientPressureValue);
       pendingAmbientPressure = false;
     }
   }
 }
 
-void setup()
-{
-  uint32_t brown_reg_temp = READ_PERI_REG(RTC_CNTL_BROWN_OUT_REG); //save WatchDog register
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+void setup() {
+  uint32_t brown_reg_temp =
+      READ_PERI_REG(RTC_CNTL_BROWN_OUT_REG); // save WatchDog register
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
 
   Serial.begin(115200);
-  Serial.println();  
+  Serial.println();
   Serial.print("CO2 Gadget Version: ");
   Serial.println(CO2_GADGET_VERSION CO2_GADGET_REV);
   Serial.println("Starting up...");
@@ -259,9 +276,9 @@ void setup()
 #endif
 
 #if defined SUPPORT_TFT
-  initDisplayTFT();  
-  displaySplashScreenTFT(); // Display init and splash screen  
-  delay(2000); // Enjoy the splash screen for 2 seconds
+  initDisplayTFT();
+  displaySplashScreenTFT(); // Display init and splash screen
+  delay(2000);              // Enjoy the splash screen for 2 seconds
   // tft.fillScreen(Black);
   tft.setTextSize(2);
 #endif
@@ -274,14 +291,13 @@ void setup()
 
 #ifdef SUPPORT_WIFI
   WiFiMulti.addAP(WIFI_SSID_CREDENTIALS, WIFI_PW_CREDENTIALS);
-  while (WiFiMulti.run() != WL_CONNECTED)
-  {
+  while (WiFiMulti.run() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
 
   /*use mdns for host name resolution*/
-  if (!MDNS.begin(UNITHOSTNAME)) { //http://esp32.local
+  if (!MDNS.begin(UNITHOSTNAME)) { // http://esp32.local
     Serial.println("Error setting up MDNS responder!");
     while (1) {
       delay(1000);
@@ -291,14 +307,14 @@ void setup()
   Serial.print(UNITHOSTNAME);
   Serial.println(".local");
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", "CO2: " + String(co2) + " PPM");
     //  server.on("/", handleRoot);      //This is display page
     //  server.on("/readADC", handleADC);//To get update of ADC Value only
   });
 
 #ifdef SUPPORT_OTA
-  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+  AsyncElegantOTA.begin(&server); // Start ElegantOTA
   Serial.println("OTA ready");
 #endif
 
@@ -318,17 +334,19 @@ void setup()
 
   Serial.println("-->[SETUP] Detecting sensors..");
 
-  sensors.setSampleTime(5);                        // config sensors sample time interval
-  sensors.setOnDataCallBack(&onSensorDataOk);      // all data read callback
-  sensors.setOnErrorCallBack(&onSensorDataError);  // [optional] error callback
+  sensors.setSampleTime(5); // config sensors sample time interval
+  sensors.setOnDataCallBack(&onSensorDataOk);     // all data read callback
+  sensors.setOnErrorCallBack(&onSensorDataError); // [optional] error callback
   sensors.setDebugMode(true);                     // [optional] debug mode
-  sensors.detectI2COnly(true);                     // force to only i2c sensors
+  sensors.detectI2COnly(true);                    // force to only i2c sensors
 
-  // sensors.scd30.setTemperatureOffset(2.0);         // example to set temp offset
+  // sensors.scd30.setTemperatureOffset(2.0);         // example to set temp
+  // offset
 
   sensors.init();
   if (sensors.isPmSensorConfigured())
-        Serial.println("-->[SETUP] Sensor configured: " + sensors.getPmDeviceSelected());
+    Serial.println("-->[SETUP] Sensor configured: " +
+                   sensors.getPmDeviceSelected());
 
   delay(500);
 
@@ -336,7 +354,8 @@ void setup()
 
   initMQTT();
 
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, brown_reg_temp); //enable brownout detector
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG,
+                 brown_reg_temp); // enable brownout detector
 
   readBatteryVoltage();
   button_init();
@@ -345,12 +364,11 @@ void setup()
   Serial.println("Ready.");
 }
 
-void loop()
-{
+void loop() {
 #ifdef SUPPORT_MQTT
   mqttClient.loop();
 #endif
-  
+
   sensors.loop();
   processPendingCommands();
 
@@ -358,14 +376,14 @@ void loop()
     if (newReadingsAvailable) {
       newReadingsAvailable = false;
 
-      nav.idleChanged=true;
+      nav.idleChanged = true;
 
-      #ifdef SUPPORT_BLE
+#ifdef SUPPORT_BLE
       gadgetBle.writeCO2(co2);
       gadgetBle.writeTemperature(temp);
       gadgetBle.writeHumidity(hum);
       gadgetBle.commit();
-      #endif
+#endif
       lastMmntTime = esp_timer_get_time();
 
       // Provide the sensor values for Tools -> Serial Monitor or Serial Plotter
@@ -378,29 +396,28 @@ void loop()
       Serial.print("Humidity[%]:");
       Serial.println(hum);
 
-//      Serial.print("Free heap: ");
-//      Serial.println(ESP.getFreeHeap());
+      //      Serial.print("Free heap: ");
+      //      Serial.println(ESP.getFreeHeap());
 
-      #ifdef SUPPORT_WIFI
+#ifdef SUPPORT_WIFI
       if (WiFiMulti.run() != WL_CONNECTED) {
         Serial.println("WiFi not connected");
       } else {
         Serial.print("WiFi connected - IP = ");
         Serial.println(WiFi.localIP());
-        if (!mqttClient.connected())
-        {
+        if (!mqttClient.connected()) {
           mqttReconnect();
         }
       }
-      #endif
+#endif
 
-      #if defined SUPPORT_MQTT && defined SUPPORT_WIFI
+#if defined SUPPORT_MQTT && defined SUPPORT_WIFI
       if (WiFiMulti.run() == WL_CONNECTED) {
         publishIntMQTT("/co2", co2);
         publishFloatMQTT("/temp", temp);
         publishFloatMQTT("/humi", hum);
       }
-      #endif
+#endif
     }
   }
 
@@ -414,5 +431,5 @@ void loop()
 #endif
 
   button_loop();
-  nav.poll(); //this device only draws when needed
+  nav.poll(); // this device only draws when needed
 }
--- CO2_Gadget_Preferences.h
+++ CO2_Gadget_Preferences.h
@@ -1,27 +1,25 @@
 #include <Preferences.h>
 Preferences preferences;
 
-void initPreferences()
-{
-    preferences.begin("CO2-Gadget", false);
-    //preferences.clear(); // Remove all preferences
-    customCalibrationValue = preferences.getUInt("customCalValue", 415);
-    altidudeMeters = preferences.getUInt("altidudeMeters", 0);
-    autoSelfCalibration = preferences.getBool("autoSelfCal", false);
-    co2OrangeRange = preferences.getUInt("co2OrangeRange", 700);
-    co2RedRange = preferences.getUInt("co2RedRange", 1000);
-    TFTBrightness = preferences.getUInt("TFTBrightness", 100);    
-    preferences.end();
+void initPreferences() {
+  preferences.begin("CO2-Gadget", false);
+  // preferences.clear(); // Remove all preferences
+  customCalibrationValue = preferences.getUInt("customCalValue", 415);
+  altidudeMeters = preferences.getUInt("altidudeMeters", 0);
+  autoSelfCalibration = preferences.getBool("autoSelfCal", false);
+  co2OrangeRange = preferences.getUInt("co2OrangeRange", 700);
+  co2RedRange = preferences.getUInt("co2RedRange", 1000);
+  TFTBrightness = preferences.getUInt("TFTBrightness", 100);
+  preferences.end();
 }
 
-void putPreferences()
-{
-    preferences.begin("CO2-Gadget", false);
-    preferences.putUInt("customCalValue", customCalibrationValue);
-    preferences.putUInt("altidudeMeters", altidudeMeters);
-    preferences.putBool("autoSelfCal", autoSelfCalibration);
-    preferences.putUInt("co2OrangeRange", co2OrangeRange);
-    preferences.putUInt("co2RedRange", co2RedRange);
-    preferences.putUInt("TFTBrightness", TFTBrightness);
-    preferences.end();
+void putPreferences() {
+  preferences.begin("CO2-Gadget", false);
+  preferences.putUInt("customCalValue", customCalibrationValue);
+  preferences.putUInt("altidudeMeters", altidudeMeters);
+  preferences.putBool("autoSelfCal", autoSelfCalibration);
+  preferences.putUInt("co2OrangeRange", co2OrangeRange);
+  preferences.putUInt("co2RedRange", co2RedRange);
+  preferences.putUInt("TFTBrightness", TFTBrightness);
+  preferences.end();
 }
