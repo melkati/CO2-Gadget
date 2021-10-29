6899494744c4c6262efa16ab83008145ed546052
eMariete.com <melkati@users.noreply.github.com>
2021-10-29 20:05:30 +0200

    Fix dupl. definition of const char *mqtt_server

--- credentials.h
+++ credentials.h
@@ -3,6 +3,6 @@
 
 #define WIFI_SSID_CREDENTIALS "MyWiFiSSID"
 #define WIFI_PW_CREDENTIALS "some_secret_password"
-const char *mqtt_server = "192.168.1.145"; // Your MQTT broker IP address if any
+// const char *mqtt_server = "192.168.1.145"; // Your MQTT broker IP address if any
 
 #endif
