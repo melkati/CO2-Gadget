930916c55a3f2eee45b13a6553dd714e4d582d5a
eMariete.com <melkati@users.noreply.github.com>
2021-10-30 14:10:59 +0200

    Separate buttons funct. into CO2_Gadget_Buttons.h

--- CO2_Gadget.ino
+++ CO2_Gadget.ino
@@ -143,34 +143,7 @@ int vref = 1100;
 /*********                                                                                   *********/
 /*****************************************************************************************************/
 // clang-format on
-#define LONGCLICK_MS 300 // https://github.com/LennartHennigs/Button2/issues/10
-#define BTN_UP 35 // Pinnumber for button for up/previous and select / enter actions
-#define BTN_DWN 0 // Pinnumber for button for down/next and back / exit actions
-#include "Button2.h"
-Button2 btnUp(BTN_UP);   // Initialize the up button
-Button2 btnDwn(BTN_DWN); // Initialize the down button
-
-void buttonsInit() {
-  btnUp.setLongClickHandler([](Button2 &b) { nav.doNav(enterCmd); });
-
-  btnUp.setClickHandler([](Button2 &b) {
-    // Up
-    nav.doNav(downCmd);
-  });
-
-  btnDwn.setLongClickHandler([](Button2 &b) { nav.doNav(escCmd); });
-
-  btnDwn.setClickHandler([](Button2 &b) {
-    // Down
-    nav.doNav(upCmd);
-  });
-}
-
-void buttonsLoop() {
-  // Check for button presses
-  btnUp.loop();
-  btnDwn.loop();
-}
+#include "CO2_Gadget_Buttons.h"
 
 /*****************************************************************************************************/
 
