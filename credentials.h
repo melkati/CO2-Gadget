fb47c5a11b35130f219182cdb2f9e4906c740abf
eMariete.com <melkati@users.noreply.github.com>
2021-11-10 20:25:27 +0100

    Add MQTT Credentials

--- credentials.h
+++ credentials.h
@@ -3,5 +3,7 @@
 
 #define WIFI_SSID_CREDENTIALS "MyWiFiSSID"
 #define WIFI_PW_CREDENTIALS "some_secret_password"
+#define MQTT_USER_CREDENTIAL "MyUser"
+#define MQTT_PW_CREDENTIAL "another_secret_password"
 
 #endif
