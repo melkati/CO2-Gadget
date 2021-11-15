1c90856b1de99a31bc088dfbcdd4fd14a4108a3c
eMariete.com <melkati@users.noreply.github.com>
2021-11-15 20:27:34 +0100

    Increase long button press to 300ms (from 200ms)

--- CO2_Gadget_Buttons.h
+++ CO2_Gadget_Buttons.h
@@ -1,7 +1,7 @@
+#include "Button2.h"
 #define LONGCLICK_MS 300 // https://github.com/LennartHennigs/Button2/issues/10
 #define BTN_UP 35 // Pinnumber for button for up/previous and select / enter actions
 #define BTN_DWN 0 // Pinnumber for button for down/next and back / exit actions
-#include "Button2.h"
 Button2 btnUp(BTN_UP);   // Initialize the up button
 Button2 btnDwn(BTN_DWN); // Initialize the down button
 
@@ -13,16 +13,16 @@ void IRAM_ATTR buttonUpISR() {
 void buttonsInit() {
   // Interrupt Service Routine to turn on the display on button UP press 
   attachInterrupt(BTN_UP, buttonUpISR, RISING);
-  
-  btnUp.setLongClickHandler([](Button2 &b) { nav.doNav(enterCmd); });
 
+  btnUp.setLongClickTime(LONGCLICK_MS);  
+  btnUp.setLongClickHandler([](Button2 &b) { nav.doNav(enterCmd); });
   btnUp.setClickHandler([](Button2 &b) {
     // Up
     nav.doNav(downCmd);
   });
 
+  btnDwn.setLongClickTime(LONGCLICK_MS);
   btnDwn.setLongClickHandler([](Button2 &b) { nav.doNav(escCmd); });
-
   btnDwn.setClickHandler([](Button2 &b) {
     // Down
     nav.doNav(upCmd);
