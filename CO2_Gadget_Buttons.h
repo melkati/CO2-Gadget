6a2cbb3efd457274f431409e293af2cb8e90b00c
eMariete.com <melkati@users.noreply.github.com>
2021-12-04 22:35:48 +0100

    Renamed LONGCLICK_MS to LONGCLICK_TIME_MS to avoid redefinition warning

--- CO2_Gadget_Buttons.h
+++ CO2_Gadget_Buttons.h
@@ -1,5 +1,5 @@
 #include "Button2.h"
-#define LONGCLICK_MS 300 // https://github.com/LennartHennigs/Button2/issues/10
+#define LONGCLICK_TIME_MS 300 // https://github.com/LennartHennigs/Button2/issues/10
 #define BTN_UP 35 // Pinnumber for button for up/previous and select / enter actions
 #define BTN_DWN 0 // Pinnumber for button for down/next and back / exit actions
 Button2 btnUp(BTN_UP);   // Initialize the up button
@@ -14,14 +14,14 @@ void buttonsInit() {
   // Interrupt Service Routine to turn on the display on button UP press 
   attachInterrupt(BTN_UP, buttonUpISR, RISING);
 
-  btnUp.setLongClickTime(LONGCLICK_MS);  
+  btnUp.setLongClickTime(LONGCLICK_TIME_MS);  
   btnUp.setLongClickHandler([](Button2 &b) { nav.doNav(enterCmd); });
   btnUp.setClickHandler([](Button2 &b) {
     // Up
     nav.doNav(downCmd);
   });
 
-  btnDwn.setLongClickTime(LONGCLICK_MS);
+  btnDwn.setLongClickTime(LONGCLICK_TIME_MS);
   btnDwn.setLongClickHandler([](Button2 &b) { nav.doNav(escCmd); });
   btnDwn.setClickHandler([](Button2 &b) {
     // Down
