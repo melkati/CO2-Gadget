9efd47f73c7633a4cd1c24270ec87d8636b9e9e9
eMariete.com <melkati@users.noreply.github.com>
2021-12-14 08:56:22 +0100

    Add compilation date & time to serial start log

--- CO2_Gadget.ino
+++ CO2_Gadget.ino
@@ -245,11 +245,14 @@ void setup() {
       READ_PERI_REG(RTC_CNTL_BROWN_OUT_REG); // save WatchDog register
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
   Serial.begin(115200);
+  delay(100);
   // Serial.printf("Total heap: %d", ESP.getHeapSize());
   // Serial.printf("Free heap: %d", ESP.getFreeHeap());
   // Serial.printf("Total PSRAM: %d", ESP.getPsramSize());
   // Serial.printf("Free PSRAM: %d", ESP.getFreePsram());
   Serial.printf("\n-->[MAIN] CO2 Gadget Version: %s%s\nStarting up...\n", CO2_GADGET_VERSION, CO2_GADGET_REV);
+  Serial.printf("\n-->[MAIN] Version compiled: %s at %s\n", __DATE__, __TIME__);
+  
   setCpuFrequencyMhz(80); // Lower CPU frecuency to reduce power consumption
   initPreferences();
   initBattery();
