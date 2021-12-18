1e00844a824c5b5620e3e157457e764a23c35be4
eMariete.com <melkati@users.noreply.github.com>
2021-12-18 22:11:39 +0100

    Fix last merge conflict

--- CO2_Gadget_Buttons.h
+++ CO2_Gadget_Buttons.h
@@ -6,7 +6,7 @@ Button2 btnUp(BTN_UP);   // Initialize the up button
 Button2 btnDwn(BTN_DWN); // Initialize the down button
 
 void IRAM_ATTR buttonUpISR() {
-  if(actualTFTBrightness==0) // Turn on the display only if it's OFF
+  if(actualDisplayBrightness==0) // Turn on the display only if it's OFF
   {
     #ifdef SUPPORT_TFT
       setTFTBrightness(DisplayBrightness); // Turn on the display at DisplayBrightness brightness
@@ -14,7 +14,7 @@ void IRAM_ATTR buttonUpISR() {
     #ifdef SUPPORT_OLED
       setOLEDBrightness(DisplayBrightness); // Turn on the display at DisplayBrightness brightness
     #endif
-    actualTFTBrightness = DisplayBrightness;
+    actualDisplayBrightness = DisplayBrightness;
     nextTimeToDisplayOff = millis() + (timeToDisplayOff*1000);
   }
 }
