843ac59226d660e54c24c30472d4cef3b052c819
eMariete.com <melkati@users.noreply.github.com>
2021-12-25 13:09:53 +0100

    Prepare for release

--- CO2_Gadget_Sensors.h
+++ CO2_Gadget_Sensors.h
@@ -36,7 +36,7 @@ void onSensorDataOk() {
   if (hum == 0.0) hum = sensors.getCO2humi();
 
   temp = sensors.getTemperature();
-  if (temp == 0.0) temp = sensors.getCO2temp();  // TODO: temp could be 0.0
+  if (temp == 0.0) temp = sensors.getCO2temp();  // TO-DO: temp could be 0.0
 
   tempFahrenheit = (temp * 1.8 + 32);
 
@@ -65,7 +65,7 @@ void initSensors() {
 
   Serial.println("-->[SENS] Detecting sensors..");
 
-  uint16_t defaultCO2MeasurementInterval = 5;  // TODO: Move to preferences
+  uint16_t defaultCO2MeasurementInterval = 5;  // TO-DO: Move to preferences
 // Breaking change: https://github.com/kike-canaries/canairio_sensorlib/pull/110
 // CanAirIO Sensorlib was multipliying sample time by two until rev 340 (inclusive). Adjust to avoid need for recalibration.
 #ifdef CSL_REVISION  // CanAirIO Sensorlib Revision > 340 (341 where CSL_REVISION was included)
