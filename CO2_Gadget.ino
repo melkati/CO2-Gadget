3a7b40808038720c795c0a12ee61f739f1fdafb1
Mario Mariete <11509521+melkati@users.noreply.github.com>
2024-01-12 06:45:23 +0100

    Fix serial console input

--- CO2_Gadget.ino
+++ CO2_Gadget.ino
@@ -381,15 +381,17 @@ void batteryLoop() {
 }
 
 void utilityLoop() {
-    if (battery_voltage > 4.5) {
+    const int16_t actualCPUFrequency = 0;
+
+    if (battery_voltage > 4.5 && actualCPUFrequency != 240) {
         Serial.flush();
         Serial.end();
-        setCpuFrequencyMhz(240);  // High CPU frecuency when working on USB power
+        setCpuFrequencyMhz(240);  // High CPU frequency when working on USB power
         Serial.begin(115200);
-    } else {
+    } else if (actualCPUFrequency != 80) {
         Serial.flush();
         Serial.end();
-        setCpuFrequencyMhz(80);  // Lower CPU frecuency to reduce power consumption
+        setCpuFrequencyMhz(80);  // Lower CPU frequency to reduce power consumption
         Serial.begin(115200);
     }
 }
@@ -437,7 +439,7 @@ void loop() {
     mqttClientLoop();
     sensorsLoop();
     readBatteryVoltage();
-    utilityLoop();
+    // utilityLoop();
     outputsLoop();
     processPendingCommands();
     readingsLoop();
