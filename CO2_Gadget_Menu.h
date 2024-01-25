5dec01f000092c5a26b67fe0d18975cd20efe46a
Mario Mariete <11509521+melkati@users.noreply.github.com>
2024-01-25 09:58:16 +0100

    Implement timeToKeepAliveMQTT

--- CO2_Gadget_Menu.h
+++ CO2_Gadget_Menu.h
@@ -140,7 +140,7 @@ result showEvent(eventMask e, navNode &nav, prompt &item) {
         case exitEvent:  // leaving navigation level
             Serial.println(" exitEvent");
             break;
-        case returnEvent:  // TO-DO:entering previous level (return)
+        case returnEvent:  // entering previous level (return)
             Serial.println(" returnEvent");
             break;
         case focusEvent:  // element just gained focus
@@ -149,10 +149,10 @@ result showEvent(eventMask e, navNode &nav, prompt &item) {
         case blurEvent:  // element about to lose focus
             Serial.println(" blurEvent");
             break;
-        case selFocusEvent:  // TO-DO:child just gained focus
+        case selFocusEvent:  // child just gained focus
             Serial.println(" selFocusEvent");
             break;
-        case selBlurEvent:  // TO-DO:child about to lose focus
+        case selBlurEvent:  // child about to lose focus
             Serial.println(" selBlurEvent");
             break;
         case updateEvent:  // Field value has been updated
