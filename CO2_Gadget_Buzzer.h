83aa125abbb13966268b7d9a01ead8feec488083
Coscolin <coscolin.sergio@gmail.com>
2024-02-03 20:29:15 +0100

    Redefine buzzerLoop() workflow

--- CO2_Gadget_Buzzer.h
+++ CO2_Gadget_Buzzer.h
@@ -8,9 +8,8 @@
 /*****************************************************************************************************/
 	
 #include <Ticker.h>  // library to async functions
-Ticker buzz;
-
-uint64_t lastTimeBuzzerBeep = 0;  // Time of last Buzzer loop
+Ticker buzz_red;
+Ticker buzz_orange;
 
 bool belowOrangeRange = true;
 bool belowRedRange = true;
@@ -27,52 +26,61 @@ void wakeUpDisplay(){
 }   
 
 void buzzerRedRange(){
-    Serial.println("[BUZZ] Buzzer RED range");
-    tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
-    delay(durationBuzzerBeep*1.3);
-    tone(BUZZER_PIN, toneBuzzerBeep+250+co2 , durationBuzzerBeep);
-    delay(durationBuzzerBeep*1.3);
-    tone(BUZZER_PIN, toneBuzzerBeep+500+co2 , durationBuzzerBeep);
+    if(co2>co2RedRange){
+        Serial.println("[BUZZ] Buzzer RED range");
+        tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
+        delay(durationBuzzerBeep*1.3);
+        tone(BUZZER_PIN, toneBuzzerBeep+250+co2 , durationBuzzerBeep);
+        delay(durationBuzzerBeep*1.3);
+        tone(BUZZER_PIN, toneBuzzerBeep+500+co2 , durationBuzzerBeep);
+    }
 }
 
 void buzzerOrangeRange(){
-    Serial.println("[BUZZ] Buzzer ORANGE range");
-    tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
-    delay(durationBuzzerBeep*1.3);
-    tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
+    if(co2>co2OrangeRange){
+        Serial.println("[BUZZ] Buzzer ORANGE range");
+        tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
+        delay(durationBuzzerBeep*1.3);
+        tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
+    }
 }
 
 void buzzerLoop(){
 
-    if(!activeBuzzer)        
-        return;
-
-    if(inMenu){  // Inside Menu stop BEEPING
-        noTone(BUZZER_PIN);
-        return;
-    }
-
-    if ((millis() - lastTimeBuzzerBeep >= timeBetweenBuzzerBeep * 1000) || (lastTimeBuzzerBeep == 0)) {
-        lastTimeBuzzerBeep = millis();
-
-        if(co2>co2RedRange){
-            if(belowRedRange || repeatBuzzer){
-                wakeUpDisplay();
-                buzz.once(0, buzzerRedRange);
-                belowRedRange = false;
-            }
-            return;
-        } else if(co2 < (co2RedRange - BUZZER_HYSTERESIS)) belowRedRange = true;
-
-        if(co2>co2OrangeRange){
-            if(belowOrangeRange || repeatBuzzer){
-                wakeUpDisplay();
-                buzz.once(0, buzzerOrangeRange);
-                belowOrangeRange = false;
-            }
-            return;
-        } else if(co2 < (co2OrangeRange - BUZZER_HYSTERESIS)) belowOrangeRange = true;
+    if(!activeBuzzer || inMenu){ // Inside Menu OR activeBuzzer=OFF stop BEEPING
+        if(buzz_red.active() || buzz_orange.active()){
+            noTone(BUZZER_PIN);
+            buzz_red.detach();
+            buzz_orange.detach();
+        }
+        belowRedRange = true;
+        belowOrangeRange = true;
         return;
     }
+
+    if(co2>co2RedRange && belowRedRange){
+        wakeUpDisplay();
+        buzz_orange.detach();
+        belowOrangeRange = true;
+        if(repeatBuzzer)  buzz_red.attach(timeBetweenBuzzerBeep, buzzerRedRange);
+        else buzz_red.once(0, buzzerRedRange);
+        belowRedRange = false;
+        return;
+    } else if(co2 < (co2RedRange - BUZZER_HYSTERESIS)){
+        buzz_red.detach();
+        belowRedRange = true;
+    } 
+
+    if(co2<co2RedRange && co2>co2OrangeRange && belowOrangeRange){
+        wakeUpDisplay();
+        if(repeatBuzzer)  buzz_orange.attach(timeBetweenBuzzerBeep, buzzerOrangeRange);
+        else buzz_orange.once(0, buzzerOrangeRange);
+        belowOrangeRange = false;
+        return;
+    } else if(co2 < (co2OrangeRange - BUZZER_HYSTERESIS)){
+        buzz_orange.detach();
+        belowOrangeRange = true;
+    } 
+    return;
 }
 #endif
