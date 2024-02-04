0c51be05a174665ea83c9406fd4c1a40b372bce6
Coscolin <coscolin.sergio@gmail.com>
2024-02-04 12:19:45 +0100

    Dar formato al documento con Shift + Alt + F

--- CO2_Gadget_Buzzer.h
+++ CO2_Gadget_Buzzer.h
@@ -25,30 +25,29 @@ void wakeUpDisplay() {
     return;
 }
 
-void buzzerRedRange(){
-    if(co2>co2RedRange){
+void buzzerRedRange() {
+    if (co2 > co2RedRange) {
         Serial.println("[BUZZ] Buzzer RED range");
-        tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
-        delay(durationBuzzerBeep*1.3);
-        tone(BUZZER_PIN, toneBuzzerBeep+250+co2 , durationBuzzerBeep);
-        delay(durationBuzzerBeep*1.3);
-        tone(BUZZER_PIN, toneBuzzerBeep+500+co2 , durationBuzzerBeep);
+        tone(BUZZER_PIN, toneBuzzerBeep + co2, durationBuzzerBeep);
+        delay(durationBuzzerBeep * 1.3);
+        tone(BUZZER_PIN, toneBuzzerBeep + 250 + co2, durationBuzzerBeep);
+        delay(durationBuzzerBeep * 1.3);
+        tone(BUZZER_PIN, toneBuzzerBeep + 500 + co2, durationBuzzerBeep);
     }
 }
 
-void buzzerOrangeRange(){
-    if(co2>co2OrangeRange){
+void buzzerOrangeRange() {
+    if (co2 > co2OrangeRange) {
         Serial.println("[BUZZ] Buzzer ORANGE range");
-        tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
-        delay(durationBuzzerBeep*1.3);
-        tone(BUZZER_PIN, toneBuzzerBeep+co2 , durationBuzzerBeep);
+        tone(BUZZER_PIN, toneBuzzerBeep + co2, durationBuzzerBeep);
+        delay(durationBuzzerBeep * 1.3);
+        tone(BUZZER_PIN, toneBuzzerBeep + co2, durationBuzzerBeep);
     }
 }
 
-void buzzerLoop(){
-
-    if(!activeBuzzer || inMenu){ // Inside Menu OR activeBuzzer=OFF stop BEEPING
-        if(buzz_red.active() || buzz_orange.active()){
+void buzzerLoop() {
+    if (!activeBuzzer || inMenu) {  // Inside Menu OR activeBuzzer=OFF stop BEEPING
+        if (buzz_red.active() || buzz_orange.active()) {
             noTone(BUZZER_PIN);
             buzz_red.detach();
             buzz_orange.detach();
@@ -58,29 +57,33 @@ void buzzerLoop(){
         return;
     }
 
-    if(co2>co2RedRange && belowRedRange){
+    if (co2 > co2RedRange && belowRedRange) {
         wakeUpDisplay();
         buzz_orange.detach();
         belowOrangeRange = true;
-        if(repeatBuzzer)  buzz_red.attach(timeBetweenBuzzerBeep, buzzerRedRange);
-        else buzz_red.once(0, buzzerRedRange);
+        if (repeatBuzzer)
+            buzz_red.attach(timeBetweenBuzzerBeep, buzzerRedRange);
+        else
+            buzz_red.once(0, buzzerRedRange);
         belowRedRange = false;
         return;
-    } else if(co2 < (co2RedRange - BUZZER_HYSTERESIS)){
+    } else if (co2 < (co2RedRange - BUZZER_HYSTERESIS)) {
         buzz_red.detach();
         belowRedRange = true;
-    } 
+    }
 
-    if(co2<co2RedRange && co2>co2OrangeRange && belowOrangeRange){
+    if (co2 < co2RedRange && co2 > co2OrangeRange && belowOrangeRange) {
         wakeUpDisplay();
-        if(repeatBuzzer)  buzz_orange.attach(timeBetweenBuzzerBeep, buzzerOrangeRange);
-        else buzz_orange.once(0, buzzerOrangeRange);
+        if (repeatBuzzer)
+            buzz_orange.attach(timeBetweenBuzzerBeep, buzzerOrangeRange);
+        else
+            buzz_orange.once(0, buzzerOrangeRange);
         belowOrangeRange = false;
         return;
-    } else if(co2 < (co2OrangeRange - BUZZER_HYSTERESIS)){
+    } else if (co2 < (co2OrangeRange - BUZZER_HYSTERESIS)) {
         buzz_orange.detach();
         belowOrangeRange = true;
-    } 
+    }
     return;
 }
 
