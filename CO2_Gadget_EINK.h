8f6ed35f570644e52fb13060b8eb77f14f09ef40
Mario Mariete <11509521+melkati@users.noreply.github.com>
2024-04-14 20:23:03 +0200

    Fix E-ink CO2 value element position for GDEH0154D67 Remove unused variables

--- CO2_Gadget_EINK.h
+++ CO2_Gadget_EINK.h
@@ -114,8 +114,6 @@ struct ElementLocations {
     int32_t co2W;
     int32_t co2H;
     u_int16_t co2FontDigitsHeight;
-    int32_t co2UnitsX;
-    int32_t co2UnitsY;
     int32_t tempXUnits;
     int32_t tempYUnits;
     int32_t tempXValue;
@@ -153,8 +151,6 @@ void setElementLocations() {
     // Common positions for most displays
     elementPosition.co2X = 0;                                                       // Left screen
     elementPosition.co2Y = (display.height() / 2) - ((display.height() - 32) / 2);  // Center text in screen. 32 is the size of 16 + 16 pixels of upper and down icon lines
-    elementPosition.co2UnitsX = display.width() - 16;                               // Right screen - height of PPM text
-    elementPosition.co2UnitsY = elementPosition.co2Y;
     elementPosition.co2W = display.width() - 16;
     elementPosition.co2H = (display.height() - 32);  // 32 is the size of 16 + 16 pixels of upper and down icon lines
     elementPosition.ppmXUnits = 16;                  // Display is rotated so vertical orientation (swaped width & height)
@@ -192,6 +188,9 @@ void setElementLocations() {
 #endif
 #if defined(EINKBOARDGDEW0213M21) || defined(EINKBOARDGDEH0154D67)
     elementPosition.co2FontDigitsHeight = 42;
+#endif
+#if defined(EINKBOARDGDEH0154D67)
+    elementPosition.co2Y = ((display.height() / 2) - ((display.height() - 32) / 2)) - (display.height() / 4);  // Center in upper half of display.
 #endif
 }
 
@@ -648,7 +647,6 @@ void displayShowValues(bool forceRedraw = false) {
 
     // testRedrawValues(true);
     showCO2(co2, elementPosition.co2X, elementPosition.co2Y, forceRedraw);
-    // showCO2units(elementPosition.co2UnitsX, elementPosition.co2UnitsY, forceRedraw);
     showTemperature(temp, elementPosition.tempXValue, elementPosition.tempYValue, forceRedraw);
     showHumidity(hum, elementPosition.humidityXValue, elementPosition.humidityYValue, forceRedraw);
     showBatteryIcon(elementPosition.batteryIconX, elementPosition.batteryIconY, true);
