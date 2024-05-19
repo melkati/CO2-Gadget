581a3e1bf5ef0054824403cd8e85080703430067
Mario Mariete <11509521+melkati@users.noreply.github.com>
2024-05-19 11:39:25 +0200

    feat: Redirect captive portal requests to /index.html

--- CO2_Gadget_WIFI.h
+++ CO2_Gadget_WIFI.h
@@ -1130,15 +1130,20 @@ class CaptiveRequestHandler : public AsyncWebHandler {
     }
 
     void handleRequest(AsyncWebServerRequest *request) {
-        AsyncResponseStream *response = request->beginResponseStream("text/html");
-        response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
-        response->print("<p>This is out captive portal front page.</p>");
-        response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
-        response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
-        response->print("</body></html>");
-        request->send(response);
-        Serial.println("-->[WiFi] Captive portal request");
+        request->redirect("/index.html");
+        Serial.println("-->[WiFi] Captive portal request redirected to /index.html");
     }
+
+    // void handleRequest(AsyncWebServerRequest *request) {
+    //     AsyncResponseStream *response = request->beginResponseStream("text/html");
+    //     response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
+    //     response->print("<p>This is out captive portal front page.</p>");
+    //     response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
+    //     response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
+    //     response->print("</body></html>");
+    //     request->send(response);
+    //     Serial.println("-->[WiFi] Captive portal request");
+    // }
 };
 
 static const void initCaptivePortal() {
