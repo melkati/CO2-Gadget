e2a6a1494e28cd72e93c47cbd2884d922f6c12cf
Mario Mariete <11509521+melkati@users.noreply.github.com>
2024-05-20 14:59:29 +0200

    feat: Add support for setting fixed IP in WEB config

--- CO2_Gadget.ino
+++ CO2_Gadget.ino
@@ -28,8 +28,8 @@ String mqttClientId = UNITHOSTNAME;
 String mqttBroker = MQTT_BROKER_SERVER;
 String mqttUser = "";
 String mqttPass = "";
-String wifiSSID = WIFI_SSID_CREDENTIALS;
-String wifiPass = WIFI_PW_CREDENTIALS;
+String wifiSSID = "";
+String wifiPass = "";
 String MACAddress = "Unset";
 uint8_t peerESPNowAddress[] = ESPNOW_PEER_MAC_ADDRESS;
 
@@ -45,6 +45,12 @@ uint64_t timeToRetryTroubledWIFI = 300;  // Time in seconds to retry WIFI connec
 uint16_t WiFiConnectionRetries = 0;
 uint16_t maxWiFiConnectionRetries = 10;
 bool wifiChanged = false;
+bool useStaticIP = false;              // Set to true if you want to use a static IP
+IPAddress staticIP(192, 168, 1, 199);  // Change this to the desired IP
+IPAddress gateway(192, 168, 1, 1);     // Change this to your network's gateway
+IPAddress subnet(255, 255, 255, 0);    // Change this to your network's subnet mask
+IPAddress dns1(8, 8, 8, 8);            // Change this to your preferred DNS server
+IPAddress dns2(8, 8, 4, 4);            // Change this to your secondary DNS server
 
 // MQTT options
 bool activeMQTT = true;
@@ -129,7 +135,6 @@ uint16_t timeToWaitForImprov = 0;  // Time in seconds to wait for improv serial
 bool captivePortalActive = false;
 uint16_t timeToWaitForCaptivePortal = 60;  // Time in seconds to wait for captive portal
 #endif
-
 
 #ifdef CUSTOM_I2C_SDA
 #undef I2C_SDA
