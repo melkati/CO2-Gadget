444af72805c8c7ed11bc32ea0862e4d6262ed323
Mario Mariete <11509521+melkati@users.noreply.github.com>
2024-05-20 15:04:03 +0200

    Revert "feat: Add support for setting fixed IP in WEB config"
    This reverts commit 4330f82bfd896468c773911a08101e99fb751fd6.

--- CO2_Gadget_WIFI.h
+++ CO2_Gadget_WIFI.h
@@ -9,6 +9,11 @@
 /*****************************************************************************************************/
 // clang-format on
 
+#if !defined WIFI_SSID_CREDENTIALS || !defined WIFI_PW_CREDENTIALS
+// If not using enviroment variables, you must have a credentials.h file
+#include "credentials.h"
+#endif
+
 #ifdef SUPPORT_CAPTIVE_PORTAL
 DNSServer dnsServer;
 #endif
@@ -724,13 +729,6 @@ String getCO2GadgetStatusAsJson() {
     doc["RSSI"] = WiFi.RSSI();
     doc["MACAddress"] = MACAddress;
     doc["hostName"] = hostName;
-    doc["useStaticIP"] = useStaticIP;
-    doc["staticIP"] = staticIP.toString();
-    doc["gateway"] = gateway.toString();
-    doc["subnet"] = subnet.toString();
-    doc["dns1"] = dns1.toString();
-    doc["dns2"] = dns2.toString();
-
 #ifdef SUPPORT_MQTT
     doc["rootTopic"] = rootTopic;
     doc["discoveryTopic"] = discoveryTopic;
@@ -1035,18 +1033,7 @@ bool connectToWiFi() {
     WiFi.disconnect(true);  // disconnect form wifi to set new wifi connection
     delay(100);
     WiFi.mode(WIFI_STA);
-
-    if (useStaticIP) {
-        if (!WiFi.config(staticIP, gateway, subnet, dns1, dns2)) {
-            Serial.println("-->[WiFi] Failed to configure static IP and DNS");
-            return false;
-        }
-        Serial.print("-->[WiFi] Configuring static IP: ");
-        Serial.println(staticIP);
-    } else {
-        WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);  // Use DHCP
-    }
-
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
     WiFi.setHostname(hostName.c_str());
     Serial.println("-->[WiFi] Setting hostname: " + hostName);
 #ifdef WIFI_PRIVACY
