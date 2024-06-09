76f03ce5179cf4be7dd6d6768a808eeaf3e52a37
Coscolin <coscolin.sergio@gmail.com>
2024-06-09 17:46:22 +0200

    Move forward declaration to CO2_Gadget.ino

--- CO2_Gadget.ino
+++ CO2_Gadget.ino
@@ -40,15 +40,16 @@
 
 /*****************************************************************************************************/
 
-#define SUPPORT_CAPTIVE_PORTAL // Please, don't disable this.
+#define SUPPORT_CAPTIVE_PORTAL  // Please, don't disable this.
 
 // Functions and enum definitions
-void reverseButtons(bool reversed);       // Defined in CO2_Gadget_Buttons.h
-void outputsLoop();                       // Defined in CO2_Gadget_Main.h
-void publishMQTTLogData(String logData);  // Defined in CO2_Gadget_MQTT.h
-void putPreferences();                    // Defined in CO2_Gadget_Preferences.h
-void menuLoop();                          // Defined in CO2_Gadget_Menu.h
-void setDisplayReverse(bool reverse);     // Defined in CO2_Gadget_TFT.h or CO2_Gadget_OLED.h or CO2_Gadget_EINK.h
+void reverseButtons(bool reversed);                 // Defined in CO2_Gadget_Buttons.h
+void outputsLoop();                                 // Defined in CO2_Gadget_Main.h
+void publishMQTTLogData(String logData);            // Defined in CO2_Gadget_MQTT.h
+void putPreferences();                              // Defined in CO2_Gadget_Preferences.h
+void menuLoop();                                    // Defined in CO2_Gadget_Menu.h
+void setDisplayReverse(bool reverse);               // Defined in CO2_Gadget_TFT.h or CO2_Gadget_OLED.h or CO2_Gadget_EINK.h
+void setDisplayBrightness(uint16_t newBrightness);  // Defined in CO2_Gadget_TFT.h or CO2_Gadget_OLED.h
 // void setBLEHistoryInterval(uint64_t interval);  // Defined in CO2_Gadget_BLE.h
 
 // Define enum for toneBuzzerBeep
