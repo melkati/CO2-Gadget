bc05ef94c200588c117815f53384264ea02c5e9f
Tazmania0 <tzmail2000@gmail.com>
2026-06-07 22:13:56 +0300

    Move save preferences to main serial menu

--- CO2_Gadget_Menu.h
+++ CO2_Gadget_Menu.h
@@ -1884,7 +1884,6 @@ MENU(configMenu, "Configuration", doNothing, noEvent, wrapStyle
   ,SUBMENU(temperatureConfigMenu)
   ,SUBMENU(displayConfigMenu)
   ,SUBMENU(outputsConfigMenu)
-  ,OP("Save preferences", doSavePreferences, enterEvent)
   ,SUBMENU(lowPowerConfigMenu)
   ,EXIT("<Back"));
 
@@ -1951,6 +1950,7 @@ MENU(mainMenu, "CO2 Gadget", doNothing, noEvent, wrapStyle
   ,SUBMENU(informationMenu)
   ,SUBMENU(configMenu)
   ,SUBMENU(calibrationMenu)
+  ,OP("Save preferences", doSavePreferences, enterEvent)
   ,SUBMENU(rebootMenu)
   ,EXIT("<Exit"));
 
